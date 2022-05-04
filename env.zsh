41daf289e33565bb1511dc1ed2d04f3e644b68c4
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2022-05-04 19:39:04 -0400

    feat: update env

--- env.zsh
+++ env.zsh
@@ -1,4 +1,5 @@
 # Add zplug and zshrc man pages.
+export MANPATH="/usr/local/man:${MANPATH}"
 export MANPATH="${ZPLUG_HOME}/doc/man:${MANPATH}"
 export MANPATH="$(dirname ${(%):-%x})/doc/man:${MANPATH}"
 
@@ -11,3 +12,35 @@ export XDG_DATA_DIRS=${XDG_DATA_DIRS:-/usr/local/share/:/usr/share/}
 
 # Set Zsh history file.
 export HISTFILE=${HISTFILE:-$XDG_CACHE_HOME/zsh/history}
+
+# Bash -> Zsh - change $PATH.
+export PATH="$HOME/bin:/usr/local/bin:/usr/bin:$PATH"
+
+# Path to oh-my-zsh installation.
+export ZSH="$HOME/.oh-my-zsh"
+
+# Lang
+export LANG=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
+export EDITOR='nvim'
+
+# Browser
+export BROWSER=wslview
+
+# Compilation flags
+export ARCHFLAGS="-arch x86_64"
+
+# NVM
+export NVM_DIR="$HOME/.config/nvm"
+[ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+# Homebrew
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# Brew - Ruby
+export PATH="$PATH:/home/linuxbrew/.linuxbrew/lib/ruby/gems/3.1.0/bin"
+
+# GH shell completion:
+eval "$(gh completion -s zsh)"
